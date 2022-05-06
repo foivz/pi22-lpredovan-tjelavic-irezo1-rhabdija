7838ac3b76ff6a274fc64d8aff5519e275706a12
DESKTOP\iva <irezo1@foi.hr>
2022-05-06 21:42:44 +0200

    initial project

---
+++ Software/FoundIT/UserControls/PrijavaUC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoundIT.UserControls
+{
+    public partial class PrijavaUC : UserControl
+    {
+        public PrijavaUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
